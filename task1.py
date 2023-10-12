4a9bf52bb952eac00ee70d36580a7fc399b86a95
Tomokim06 <tomokim048@deltalearns.ca>
2023-10-12 13:30:23 -0700

    working on task 2

--- task1.py
+++ task1.py
@@ -5,4 +5,20 @@
 Use a for loop to iterate through the list of numbers.
 If the number is an even number print it out.
 """
-numbers = [3,19,3,6,3,6,7,8,5,4,6,78,0]
+#numbers = [3,19,3,6,3,6,7,8,5,4,6,78,0]
+
+
+#for i in numbers:
+  #  if i%2 == 0:
+  #      print(i)
+
+nameList = ["Lebron","Kobe","Michale","Shaq","Dennis"]
+
+q = "Enter a name: "
+a = str(input(q))
+
+for i in nameList:
+    if a == nameList:
+        print("That name is on the list")
+    else:
+        print("That name is not on the List")
--- task2.py
+++ task2.py
@@ -2,7 +2,8 @@
 """
 ###### Task 2
 Ask the user to enter a name.
-Check the name against a tuple that contains a series of names to see if it is a match. Use a for loop this time instead of a single if with multiple
+Check the name against a tuple that contains a series of names to see if it is a match. 
+Use a for loop this time instead of a single if with multiple
 logical operators
 (2 points)
 
@@ -22,4 +23,15 @@ Enter a name: Lebron
 That name is on the list
 """
 
-nameList = ("Lebron","Kobe","Michale","Shaq","Dennis")
+nameList = ["Lebron","Kobe","Michale","Shaq","Dennis"]
+
+q = "Enter a name: "
+a = input(q)
+
+for i in nameList:
+    if a == i[0] or a == i[1] or a == i[2] or a == i[3] or a == i[4]:
+        print("That name is on the list")
+        break
+    else:
+        print("That name is not on the List")
+        break
